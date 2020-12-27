c01dfefccf6be6daa3abc32f79c128f9b3368b54
phanet noch <phanet@imake.co.th>
2020-12-27 19:33:43 +0700

    added save product into local storage

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.css"],
 })
 export class HomePageComponent implements OnInit {
-// step 1 
 
-// create a list for add to local storage 
-  lstProduct:any = [];
-  
-  // create object of product 
-  objProduct:any = {
-    name:"",
-    qty:0,
-    price:0
-    
+  lstProduct: any = []; //created for loop in view(UI) 
+  lstLocalStorage:any = [];
+
+  objProduct: any = {
+    name: "",
+    qty: 0,
+    price: 0,
   };
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+   let jsonStr = localStorage.getItem('$product'); // getting data from local storage 
+   if(jsonStr)// if has data 
+   {
+    let products = JSON.parse(jsonStr); // convert from string to json object back 
+
+    this.lstProduct = products;   //
+   }
+  
   }
 
-  onSaveProduct()
-  {
-    this.lstProduct.push(this.objProduct);
-    console.log(this.objProduct);
+  onSaveProduct() {
+    this.lstLocalStorage = [];
+    this.lstLocalStorage = this.lstProduct;
+    this.lstLocalStorage.push(this.objProduct);
+    localStorage.setItem("$product", JSON.stringify(this.lstLocalStorage));
   }
 
+  onDelete(index: number) {
+    console.log(index);
+
+    this.lstProduct.splice(index);
+  }
 }
